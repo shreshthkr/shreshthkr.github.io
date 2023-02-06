fd42dade01dc56f41b9633b8166f6b07f794f20e
shreshthkr <shreshthkumar6@gmail.com>
2023-02-06 22:19:29 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      This is my Portfolio
+    <Navbar />
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div id='nav-menu'>Navbar</div>
+    <div id='nav-menu'>
+      <div id="nav_name_div">
+        <h1 href="" id="nav_name" >Shreshth</h1>
+      </div>
+      <div id='nav_components'>
+        <p class="nav-link home"> <a href="#home">Home</a></p>
+        <p class="nav-link about"> <a href="#about">About</a></p>
+        <p class="nav-link skills"> <a href="#skills">Skills</a></p>
+        <p  class="nav-link projects"> <a href="#projects"> Projects</a></p>
+        <p class="nav-link contact"> <a href="#contact"> Contact</a></p>
+      </div>
+      <div class="nav-link resume">
+        <button id="resume_dwnld">Resume</button>
+      </div>
+    </div>
   )
 }
 
 export default Navbar;
+
+
+
+//#FF6A3D
+//#F4DB7D
