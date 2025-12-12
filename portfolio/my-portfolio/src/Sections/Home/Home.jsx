7c3765eb12d3daa4cbed49cb232f1bbba093b64a
shreshthkr <shreshthkumar6@gmail.com>
2025-12-12 15:50:56 +0530

    fixed resume button

--- portfolio/my-portfolio/src/Sections/Home/Home.jsx
+++ portfolio/my-portfolio/src/Sections/Home/Home.jsx
@@ -1,55 +1,66 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
-import { Type } from '../../Components/Type';
-import {BsGithub,BsLinkedin} from "react-icons/bs";
-import {SiGmail} from "react-icons/si";
-import {FaPhoneAlt} from "react-icons/fa";
-import {HiDownload} from "react-icons/hi";
-import Shreshth_Kumar_Resume from "../../Components/Downloads/Shreshth_Kumar_Resume.pdf";
+import { Type } from "../../Components/Type";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import { FaPhoneAlt } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 const Home = () => {
   const handleClick = () => {
     const url =
-      "https://drive.google.com/file/d/1sgv3hFs1pO94RJQb6hc6vY47oVaG-XaF/view?usp=sharing";
+      "https://drive.google.com/uc?export=download&id=1sgv3hFs1pO94RJQb6hc6vY47oVaG-XaF";
+
+    window.open(url, "_blank", "noopener,noreferrer");
+
     const link = document.createElement("a");
     link.href = url;
     link.download = "Shreshth_Kumar_Resume.pdf";
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
   };
+
   const handleOpenMailPage = () => {
-    window.open('mailto:shreshthkumar6.com');
+    window.open("mailto:shreshthkumar6@gmail.com");
   };
   const handleGit = () => {
     window.open("https://github.com/shreshthkr");
   };
   const handlePhoneClick = () => {
-    window.open('tel:+918700244923'); 
+    window.open("tel:+918700244923");
   };
-   const handleLikedin = () => {
+  const handleLinkedin = () => {
     window.open("https://www.linkedin.com/in/shreshthkumar1263/");
   };
+
   return (
     <>
-     <div id="home">
-        <div id='heading'>
-          <h1 id='name'>Hello, I am <span>Shreshth Kumar</span></h1>
-            <h1 id='type'><Type /></h1>
+      <div id="home">
+        <div id="heading">
+          <h1 id="name">
+            Hello, I am <span>Shreshth Kumar</span>
+          </h1>
+          <h1 id="type">
+            <Type />
+          </h1>
         </div>
+
         <div id="resume-btn-2">
-          <a   id="resume-link-2"
-                href={Shreshth_Kumar_Resume}
-                download="Shreshth_Kumar_Resume"><button onClick={handleClick}><HiDownload /> Resume</button></a>
-           
+          <button id="resume-link-2" onClick={handleClick}>
+            <HiDownload /> Resume
+          </button>
         </div>
+
         <div id="social-links">
-        <BsGithub className='social-icons' onClick={handleGit} /> 
-        <SiGmail className='social-icons' onClick={handleOpenMailPage}  />
-        <FaPhoneAlt className='social-icons' onClick={handlePhoneClick}/>
-        <BsLinkedin className='social-icons' onClick={handleLikedin}/>
+          <BsGithub className="social-icons" onClick={handleGit} />
+          <SiGmail className="social-icons" onClick={handleOpenMailPage} />
+          <FaPhoneAlt className="social-icons" onClick={handlePhoneClick} />
+          <BsLinkedin className="social-icons" onClick={handleLinkedin} />
         </div>
-        </div> 
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
